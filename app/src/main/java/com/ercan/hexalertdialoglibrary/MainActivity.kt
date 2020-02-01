fc88e52274868c61b00ce9e19b16e78aa7f9067c
Ercan Akkök <ercanakkok@gmail.com>
2020-02-01 20:08:25 +0300

    v1.1

--- app/src/main/java/com/ercan/hexalertdialoglibrary/MainActivity.kt
+++ app/src/main/java/com/ercan/hexalertdialoglibrary/MainActivity.kt
@@ -2,11 +2,28 @@ package com.ercan.hexalertdialoglibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ercan.hexalertdialog.HexAlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        helloTw.setOnClickListener {
+
+            HexAlertDialog(this).apply {
+                title = "Title"
+                message = "This is the message."
+                positiveBtnClickListener = {
+                    // Do some work here.
+                }
+                negativeBtnClickListener = {
+                    // Do some work here.
+                }
+            }.show()
+
+        }
     }
 }
--- hexalertdialog/src/main/java/com/ercan/hexalertdialog/Animation.kt
+++ hexalertdialog/src/main/java/com/ercan/hexalertdialog/Animation.kt
@@ -1,9 +1,9 @@
 package com.ercan.hexalertdialog
 
-enum class Animation {
-    BOTTOM_TO_TOP,
-    TOP_TO_BOTTOM,
-    LEFT_TO_RIGHT,
-    RIGHT_TO_LEFT,
-    TRANSPARENT_TO_OPAQUE
+enum class Animation(val resId: Int) {
+    BOTTOM_TO_TOP(R.style.BottomToTopTheme),
+    TOP_TO_BOTTOM(R.style.TopToBottomTheme),
+    LEFT_TO_RIGHT(R.style.LeftToRightTheme),
+    RIGHT_TO_LEFT(R.style.RightToLeftTheme),
+    TRANSPARENT_TO_OPAQUE(R.style.TransparentToOpaqueTheme)
 }
